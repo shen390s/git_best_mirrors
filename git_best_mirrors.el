7fe225a610edd275edadd1fa0e9267d2dfd77f87
Rongsong Shen <shenrs@cn.ibm.com>
2021-05-27 11:13:56 +0800

    used by emacs

---
+++ git_best_mirrors.el
@@ -0,0 +1,7 @@
+;; git_best_mirrors.el --- use best mirror for china users -*- lexical-binding: t -*-
+
+;;;###autoload
+(defun git_best_mirrors ()
+  (message "git_best_mirrors is ready!"))
+
+(provide 'git_best_mirrors)
